4d52cebe46c4c201983456cb812f3279f957c95a
aguscarrera77 <aguscarrera77@gmail.com>
2025-06-26 12:26:42 -0300

    Actual

---
+++ Javascript/java.js
@@ -0,0 +1,16 @@
+console.log("Hola a todos este es mi primer mensaje");
+let name="Curso Programacion";
+const curso= 10;
+console.log(name);
+
+let nombre= prompt("Cual es tu nombre");
+
+
+alert("Bienvenido a mi pagina");
+
+let confirmar=confirm("Desea continuar en nuestro sitio?");
+
+
+
+
+
