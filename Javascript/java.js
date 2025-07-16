2ededd40a9b71fb395b5f92e2138ee2883525806
aguscarrera77 <aguscarrera77@gmail.com>
2025-07-16 11:14:15 -0300

    Actual

--- Javascript/java.js
+++ Javascript/java.js
@@ -1,16 +1,35 @@
-console.log("Hola a todos este es mi primer mensaje");
-let name="Curso Programacion";
-const curso= 10;
-console.log(name);
+/*let user = "agustin";
+let password = "secreta";
+let acceso = false;
 
-let nombre= prompt("Cual es tu nombre");
+for (let i = 1; i <= 3; i++) {
+  let usuario = prompt("Ingresá tu nombre de usuario:");
+  let clave = prompt("Ingresá tu clave:");
 
+  if (usuario === user && clave === password) {
+    alert(" Bienvenido, " + usuario + "!");
+    acceso = true;
+    break; // termina el for si acierta.
+  } else {
+    alert("Usuario o clave incorrectos. Intento " + i + " de 3");
+  }
+}
 
-alert("Bienvenido a mi pagina");
+if (!acceso) {
+  alert("Acceso bloqueado por fallos consecutivos.");
+}*/
+/*let password='';
 
-let confirmar=confirm("Desea continuar en nuestro sitio?");
+while(password!=='admin'){
+password=prompt('Ingrese su clave');
 
+}
+alert('Usted ingreso al sitio.');*/
+let clave="";
+let intentos=0;
 
-
-
-
+while(clave!=="agustin"){
+clave=prompt('Ingrese la clave :');
+intentos++;
+}
+alert('Bienvenido '+ intentos+ 'luego de 3 intentos.')
